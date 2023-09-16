be9da2ac25da828da0475f527ea43f9825307da0
Yomna Osama <yabufrikha@gmail.com>
2023-09-17 01:25:55 +0300

    Updated colors palette

---
+++ src/constants/colors.ts
@@ -0,0 +1,12 @@
+export default {
+  white: '#ffffff',
+  black: '#202020',
+  primary: '#d9201a',
+  secondary: "#a5a5a5",
+  shadow: '#a8bdf4',
+  primaryText: "#47464b",
+  secondaryText: "#a4a4ad",
+  dividerColor: '#f0eff4',
+  placeholder: "#b1b1bc",
+  headerBackground: "#fafafa",
+}
